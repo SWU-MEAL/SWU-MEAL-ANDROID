7dde6c56232592c124819c2675a980235f2d61e1
yerim123456 <yearim1226@gmail.com>
2023-12-18 02:10:06 +0900

    [chore] : update app version

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,8 +1,8 @@
 object AppConfig {
-    const val PACKAGE_NAME = "com.example.swubab"
+    const val PACKAGE_NAME = "com.swubab"
     const val COMPILE_SDK = 33
     const val TARGET_SDK = 33
     const val MIN_SDK = 26
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "1.2"
 }
